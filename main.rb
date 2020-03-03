5d7da153b7feeed1f42b54b229e4f72144a3264d
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-03-02 19:32:27 -0800

    refactor, implement turns and rounds

--- main.rb
+++ main.rb
@@ -18,46 +18,52 @@ class Game
 
   p1 = Player.new(player_1)
   p2 = Player.new(player_2)
-
+  
+  player = p2
+  round = 1
   while p1.lives > 0 && p2.lives > 0
-    puts "----------- New Turn -----------"
-    q1 = Question.new
-    puts "#{p1.name}'s question:"
-    puts q1.prompt
-
-    print "#{player_1} answer: "
-    p1_answer = gets.chomp.to_i
-    puts "--------------------------------"
-
-    q2 = Question.new
-    puts "#{p2.name}'s question:"
-    puts q2.prompt 
-
-    print "#{player_2} answer: "
-    p2_answer = gets.chomp.to_i
-    puts "------- Question Result --------"
-
-    if p1_answer != q1.answer
-      p1.lives -= 1
-      puts "#{p1.name} answered incorrect"
-    elsif p2_answer != q2.answer
-      p2.lives -= 1
-      puts "#{p2.name} answered incorrect"
-    else 
-      puts "Both players answered correctly"
+    player == p2 ? player = p1 : player = p2
+    if player == p1
+      puts "--------------------------------"
+      puts "|           Round #{round}            |"
+      puts "--------------------------------"
     end
+
+    q = Question.new
+    puts "#{player.name}'s question:"
+    puts q.prompt
+
+    print "#{player.name} answer: "
+    player_answer = gets.chomp.to_i
     
-    puts "------------ Score -------------"
-    puts "#{p1.name} has #{p1.lives} remaining"
-    puts "#{p2.name} has #{p2.lives} remaining"
+    puts "--------------------------------"
+    if player_answer != q.answer
+      player.lives -= 1
+      puts "#{player.name} answered incorrect!"
+    else
+      puts "CORRECTAMUNDO!"
+    end
     puts "--------------------------------"
 
+    
+    if player == p2 
+      puts "--------------------------------"
+      puts "|       Round #{round} result        |"
+      puts "--------------------------------"
+      puts "#{p1.name} has #{p1.lives} remaining"
+      puts "#{p2.name} has #{p2.lives} remaining"
+      puts "--------------------------------"
+      puts "################################"
+
+      round += 1
+    end
   end
 
+  puts "#         Game Over            #"
   puts "################################"
-  if p1.lives = 0 
+  if p1.lives == 0 
     puts "#{p2.name} wins!"
-  elsif p2.lives = 0
+  elsif p2.lives == 0
     puts "#{p1.name} wins!"
   end
   puts "################################"
